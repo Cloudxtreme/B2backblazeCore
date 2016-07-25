d05806997199e84c5bfb7382bd19252a27f73043
James Kozlowski <jameskozlowski@gmail.com>
2016-07-24 22:39:57 -0300

    Update Program.cs
    Forgot to remove id and key, removed, and reset my key to prevent use

--- src/APITest/Program.cs
+++ src/APITest/Program.cs
@@ -10,7 +10,7 @@ namespace APITest
         public static void Main(string[] args)
         {
             B2API.B2API t = new B2API.B2API();
-            bool r = t.AuthorizeAccount("5685b21a6d74", "001eed63dc3b61da597522e219f1e2d0299e9c02dc").Result;
+            bool r = t.AuthorizeAccount("", "").Result;
             List<B2API.B2Bucket> buckets =  t.ListBuskets().Result;
             List<B2API.B2File> files = t.ListFiles(buckets[1]).Result;
             var dl = t.DownloadFile(buckets[1], files[0], "test.jpg");
